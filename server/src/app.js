201c548768bd2ade2855ae27890c98b31e8b6361
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2021-05-29 13:11:33 +0800

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const db = require("./utils/db");
+const models = require("./models");
+
+const app = express();
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("tiny"));
+
+// DATABASE CONNECTION
+db.authenticate()
+	.then(() => {
+		db.sync({ alter: true }).then(() => {
+			console.log("Database is sync");
+			console.log("Database connected");
+		});
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
+// ERROR HANDLING
+
+app.use((req, res, next) => {
+	const error = new Error("Not Found");
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	error.status = error.status || 500;
+	res.status(error.status).json({ error_message: error.message });
+});
+
+// LISTEN
+app.listen(process.env.PORT || 3000, () => {
+	console.log("Server is listening");
+});
